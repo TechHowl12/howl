37e97de91a70586f5879173be08499d331b8eb26
codewithafzall <afzalshaikh19992@gmail.com>
2024-12-05 11:47:37 +0530

    mobile header links

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -110,14 +110,14 @@ export const Header = () => {
 
         {/* Mobile Menu */}
         <div
-          className={`fixed top-0  right-0 h-[100vh] w-full text-white bg-[#2B2631] opacity-[95%] z-40 transform ${
+          className={`fixed top-0 right-0 h-[100vh] w-full flex justify-start items-end text-black uppercase bg-[#fff] opacity-[95%] z-40 transform ${
             isOpen ? "translate-x-0" : "translate-x-full"
           } transition-transform duration-300 ease-in-out md:hidden`}
         >
           {/* Close Button */}
           <button
             onClick={() => setIsOpen(false)}
-            className="absolute top-7 right-6 text-white inline-flex items-center justify-center p-2 bg-[#2B2631] opacity-[60%] border-2 rounded-tl-xl rounded-br-xl z-50"
+            className="absolute top-7 right-6 text-black inline-flex items-center justify-center p-2 bg-[#fff] opacity-[60%] border-2 border-black rounded-tl-xl rounded-br-xl z-50"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -125,7 +125,7 @@ export const Header = () => {
               viewBox="0 0 24 24"
               stroke="currentColor"
               strokeWidth="2"
-              className="w-7 h-7 text-white"
+              className="w-7 h-7 text-black"
             >
               <path
                 strokeLinecap="round"
@@ -136,32 +136,32 @@ export const Header = () => {
           </button>
 
           {/* Mobile Navigation Links */}
-          <div className="px-2 pt-20 pb-3 space-y-1 sm:px-3">
+          <div className="px-2 pb-3 space-y-1 sm:px-3">
             <a
               href="https://pages.howl.in/work/"
               target="_blank"
-              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-primary-foreground hover:text-primary"
+              className="block px-3 py-4 rounded-md text-5xl bold font-medium hover:bg-primary-foreground hover:text-primary"
             >
               Work
             </a>
             <a
               href="https://pages.howl.in/about/"
               target="_blank"
-              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-primary-foreground hover:text-primary"
+              className="block px-3 py-6 rounded-md text-5xl bold font-medium hover:bg-primary-foreground hover:text-primary"
             >
               About
             </a>
             <a
               href="https://pages.howl.in/careers/"
               target="_blank"
-              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-primary-foreground hover:text-primary"
+              className="block px-3 py-6 rounded-md text-5xl bold font-medium hover:bg-primary-foreground hover:text-primary"
             >
               Careers
             </a>
             <a
               href="https://pages.howl.in/contact/"
               target="_blank"
-              className="block px-3 py-2 rounded-md text-base font-medium hover:bg-primary-foreground hover:text-primary"
+              className="block px-3 py-6 rounded-md text-5xl bold font-medium hover:bg-primary-foreground hover:text-primary"
             >
               Contact
             </a>
